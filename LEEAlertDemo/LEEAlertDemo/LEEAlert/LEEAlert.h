9a9f6b8edb41fcb53eb419f58679fe603c94f25b
李响 <applelixiang@126.com>
2016-04-22 15:38:13 +0800

    完善注释

--- LEEAlertDemo/LEEAlertDemo/LEEAlert/LEEAlert.h
+++ LEEAlertDemo/LEEAlertDemo/LEEAlert/LEEAlert.h
@@ -34,7 +34,7 @@ typedef LEEAlertConfigModel *(^LEEConfigAlertToViewController)(UIViewController 
  *
  * 在使用LEEAlert的过程中如果出现bug请及时以以下任意一种方式联系我，我会及时修复bug
  *
- * QQ    : 可以添加SDAutoLayout群 497140713 在这里找我到(LEE)
+ * QQ    : 可以添加SDAutoLayout群 497140713 在这里找到我(LEE)
  * Email : applelixiang@126.com
  * GitHub: https://github.com/lixiang1994/LEEAlert
  * 简书:    http://www.jianshu.com/users/a6da0db100c8
