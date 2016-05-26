322e3a8ab4b6959025c369d4d9c508865f44c8bd
李响 <applelixiang@126.com>
2016-05-26 12:48:31 +0800

    完善分享demo 增加分页

--- LEEAlertDemo/LEEAlertDemo/DemoView/ShareView/ShareView.h
+++ LEEAlertDemo/LEEAlertDemo/DemoView/ShareView/ShareView.h
@@ -1,7 +1,7 @@
 
 /*!
  *  @header ShareView.h
- *          LEEActionSheetDemo
+ *          LEEAlertDemo
  *
  *  @brief  分享视图
  *
@@ -34,6 +34,21 @@ typedef enum {
  *  打开分享Block
  */
 @property (nonatomic , copy ) void (^OpenShareBlock)(ShareType type);
+
+/**
+ *  初始化分享视图
+ *
+ *  @param frame          frame
+ *  @param infoArray      信息数组
+ *  @param maxLineNumber  最大行数
+ *  @param maxSingleCount 单行最大个数
+ *
+ *  @return 分享视图对象
+ */
+- (instancetype)initWithFrame:(CGRect)frame
+                    InfoArray:(NSArray *)infoArray
+                MaxLineNumber:(NSInteger)maxLineNumber
+               MaxSingleCount:(NSInteger)maxSingleCount;
 
 @end
 
